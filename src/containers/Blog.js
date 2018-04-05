ed0d1bdb70e38ac7a8cfa854cae89c80865b87eb
Tanner Linsley <tannerlinsley@gmail.com>
2018-04-05 14:53:51 -0600

    Added Pages to Blog with Paginator

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,13 +1,19 @@
-
 import React from 'react'
 import { withRouteData, Link } from 'react-static'
+import styled from 'styled-components'
 //
 
-export default withRouteData(({ posts }) => (
+const PaginationLink = styled(({ isCurrent, ...rest }) => <Link {...rest} />)`
+  display: inline-block;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  margin: 0 0.5rem 0.5rem 0;
+  padding: 0.2rem 0.3rem;
+  opacity: ${props => (props.isCurrent ? 0.5 : 1)};
+`
+
+export default withRouteData(({ posts, currentPage, totalPages }) => (
   <div>
     <h1>It's blog time.</h1>
-    <br />
-    All Posts:
     <ul>
       {posts.map(post => (
         <li key={post.id}>
@@ -15,5 +21,19 @@ export default withRouteData(({ posts }) => (
         </li>
       ))}
     </ul>
+
+    <h5>Pages</h5>
+    <div>
+      {Array.from(new Array(totalPages), (d, i) => i).map(page => {
+        page += 1
+        return (
+          <span key={page}>
+            <PaginationLink to={`/blog/page/${page}`} isCurrent={page === currentPage}>
+              {page}
+            </PaginationLink>
+          </span>
+        )
+      })}
+    </div>
   </div>
 ))
