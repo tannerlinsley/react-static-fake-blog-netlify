d40007c632941ff1a071bbfe558cfe90d68bfbaa
Tanner Linsley <tannerlinsley@gmail.com>
2018-04-05 14:54:01 -0600

    Removed all posts link from Post

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -4,8 +4,6 @@ import { withRouteData, Link } from 'react-static'
 
 export default withRouteData(({ post, user }) => (
   <div>
-    <Link to="/blog/">{'<'} All Posts</Link>
-    <br />
     <h3>{post.title}</h3>
     <h5>
       By <Link to={`/users/${user.username}`}>{user.name}</Link>
