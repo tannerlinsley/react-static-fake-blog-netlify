8399ceae9cc463ca965c6a8fd21e752d905f5e46
Tanner Linsley <tannerlinsley@gmail.com>
2018-04-05 14:10:42 -0600

    Added the user to each post

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { withRouteData, Link } from 'react-static'
 //
 
-export default withRouteData(({ post }) => (
+export default withRouteData(({ post, user }) => (
   <div>
-    <Link to="/blog/">{'<'} Back</Link>
+    <Link to="/blog/">{'<'} All Posts</Link>
     <br />
     <h3>{post.title}</h3>
+    <h5>
+      By <Link to={`/users/${user.username}`}>{user.name}</Link>
+    </h5>
     <p>{post.body}</p>
   </div>
 ))
